b3be05812ca50d90dd3ea2ef287effd57c0e8468
Honmasu <112128372+Honmasu@users.noreply.github.com>
2024-03-16 19:18:46 -0400

    cleanage
    enlever les truc de trop et qui ne marche pas

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript to initialize Swiper
-var swiperFilters = new Swiper('.swiper-filters', {
-    // Swiper parameters
-    slidesPerView: 'auto', // Display as many slides as possible without space between them
-    spaceBetween: 10, // Add a small space between slides
-    // Add other parameters as needed
-  });
