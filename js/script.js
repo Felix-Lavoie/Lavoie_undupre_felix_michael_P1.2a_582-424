7a0d348b559fe903ce9aedcda374b7c21295b9f3
Honmasu <112128372+Honmasu@users.noreply.github.com>
2024-03-17 18:46:19 -0400

    swiper de l'app
    ajout de swiper de l'app et fix le paddding et espace

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-// Initialize Swiper
-var swiperScreenshots = new Swiper('.swiper-screenshots', {
-      slidesPerView: 1,
-      centeredSlides: true,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        1200: {
-          slidesPerView: 3,
-        }
-      }
-});
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    freeMode: true,
+  });
